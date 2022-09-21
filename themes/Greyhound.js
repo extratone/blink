5b12e85f05e79f96008557632beaf484a2560b0b
David Blue <davidblue@extratone.com>
2022-09-21 13:52:24 -0500

    #5

---
+++ themes/Greyhound.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',[ "#1C1C1D" ,  "#EC6608" ,  "#D2386C" ,  "#F39200" ,  "#666666" ,  "#666667" ,  "#5A5A5E" ,  "#B2B2B2" ,  "#5A5A5E" ,  "#EC6608" ,  "#F88148" ,  "#F39200" ,  "#6C7179" ,  "#77808C" ,  "#939597" ,  "#D0D0D0" ]);
+t.prefs_.set('foreground-color',  "#F39200" );
+t.prefs_.set('background-color',  "#1C1C1D" );
+t.prefs_.set('cursor-color', 'rgba(248, 129, 72,0.5)');
